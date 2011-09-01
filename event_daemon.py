11971d0b3e184ea4bf9fe860f6c3741c60ab4398
Rob Knapp <rob@bizling.com>
2011-08-31 20:03:15 -0400

    Twitter reader/event daemon now working.

--- event_daemon.py
+++ event_daemon.py
@@ -9,20 +9,27 @@ import send_event
 import bro_reader
 import snort_reader
 import syslog_reader
-
+import twitter_reader
 import sys
 
-host = sys.argv[1]
-port = int(sys.argv[2])
-if sys.argv[3] == "bro":
-    reader = bro_reader.BroReader()
-elif sys.argv[3] == "snort":
-    reader = snort_reader.SnortReader()
-elif sys.argv[3] == "syslog":
-    reader = syslog_reader.SyslogReader()
-else:
-    print "Usage: event_daemon.py IP port [bro|snort|syslog]"
-socket = send_event.EncapsulateForPanda()
-for line in sys.stdin:
-    message = reader.format(line)
-    socket.send_event(host, port, message)
+if len(sys.argv)>=4:
+    host = sys.argv[1]  
+    port = int(sys.argv[2])
+    if sys.argv[3] == "bro":
+        reader = bro_reader.BroReader()
+    elif sys.argv[3] == "snort":
+        reader = snort_reader.SnortReader()
+    elif sys.argv[3] == "syslog":
+        reader = syslog_reader.SyslogReader()
+    elif sys.argv[3] == 'twitter':
+        reader = twitter_reader.TwitterReader(host, port)
+        reader.run()
+    if reader:
+        socket = send_event.EncapsulateForPanda()
+        for line in sys.stdin:
+            message = reader.format(line)
+            socket.send_event(host, port, message)
+    else:
+            reader.run()
+print "Usage: event_daemon.py IP port [bro|snort|syslog|twitter]"
+
--- twitter_reader.py
+++ twitter_reader.py
@@ -2,13 +2,55 @@
 #shown below
 from oauth_tokens import *
 import twitter
+import time 
+import re
+import send_event
 
-if __name__=="__main__":
-    a = twitter.Api(CONSUMER_KEY, CONSUMER_SECRET, USER_ACCESS_TOKEN,
-        ACCESS_TOKEN_SECRET)
-    followers = a.GetFollowers()
-    for f in followers:
-        print f.name
+def findMentions(tweet):
+    iter =  re.finditer(r'(\A|\s)@(\w+)', tweet)
+    return iter
+class TwitterReader():
+
+    def __init__(self, ip, port):
+        self._port  = port
+        self._ip = ip
+    def format(self, u):
+        if self.since_id==None or u.GetId() > self.since_id:
+            self.since_id = u.GetId()
+        timesent = u.GetCreatedAtInSeconds()
+        if u.text.find("@") > 0:
+            type = "Mention"
+            mentions = list()
+            for m in findMentions(u.text):
+                mentions.append(m.group(0))
+                
+            mentions = ",".join(mentions)
+        else:
+            type = "Status"
+            mentions = ""
+            
         
-    for u in a.GetFriendsTimeline():
-        print u.user.name + u" -- " + u.GetText()
+        return "|".join([str(timesent), type, "ALARM", u.user.screen_name, "N/A", mentions, "N/A", u.text])
+
+    def run(self):
+        a = twitter.Api(CONSUMER_KEY, CONSUMER_SECRET, USER_ACCESS_TOKEN,
+        ACCESS_TOKEN_SECRET)
+        self.since_id = None
+        socket = send_event.EncapsulateForPanda()
+        while 1:
+            try:
+                statuses = a.GetFriendsTimeline(since_id=self.since_id)
+                for status in statuses:
+                  message = self.format(status)
+                  socket.send_event(self._ip, self._port, message.encode("ascii", 'replace'))
+                  print message
+            except:
+                pass # most likely a connection error...so lets try again later...
+            print "sleeping..."
+            time.sleep(60)
+if __name__=="__main__":
+    reader = TwitterReader()
+    reader.run()
+
+
+        
